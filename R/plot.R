259b1d1ea3eac594fd0f9e30833dfdaac8cacda3
Daniel Lüdecke <mail@danielluedecke.de>
2019-07-26 11:59:50 +0200

    d'oh...

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,5 @@
 #' @export
-plot.see_estimate_contrasts <- function(x, ...) {
+plot.estimate_contrasts <- function(x, ...) {
   if (!requireNamespace("see", quietly = TRUE)) {
     stop("Package 'see' needed to plot results from equivalence-test. Please install it by running `install.packages('see')`.")
   }
