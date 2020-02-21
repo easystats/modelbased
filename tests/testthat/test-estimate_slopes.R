2972364ec9e0b296243efd963e6886a2c92bd7fe
DominiqueMakowski <dom.mak19@gmail.com>
2020-02-21 23:20:53 +0800

    uncomment tests #39

--- tests/testthat/test-estimate_slopes.R
+++ tests/testthat/test-estimate_slopes.R
@@ -1,17 +1,14 @@
-context("estimate_slopes")
+testthat::test_that("estimate_slopes", {
+  library(insight)
+  library(rstanarm)
 
+  testthat::expect_error(estimate_slopes(insight::download_model("stanreg_lm_1")))
 
-# testthat::test_that("estimate_slopes", {
-#   library(insight)
-#   library(rstanarm)
-#
-#   testthat::expect_error(estimate_slopes(insight::download_model("stanreg_lm_1")))
-#
-#   model <- insight::download_model("stanreg_lm_4")
-#   estim <- estimate_slopes(model)
-#   testthat::expect_equal(c(nrow(estim), ncol(estim)), c(1, 7))
-#
-#   model <- insight::download_model("stanreg_lm_6")
-#   estim <- estimate_slopes(model)
-#   testthat::expect_equal(c(nrow(estim), ncol(estim)), c(3, 7))
-# })
+  model <- insight::download_model("stanreg_lm_4")
+  estim <- estimate_slopes(model)
+  testthat::expect_equal(c(nrow(estim), ncol(estim)), c(1, 7))
+
+  model <- insight::download_model("stanreg_lm_6")
+  estim <- estimate_slopes(model)
+  testthat::expect_equal(c(nrow(estim), ncol(estim)), c(3, 7))
+})
