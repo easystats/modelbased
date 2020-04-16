3548158a5e19d8b474ca4ee533194cf0fe2c7017
Mattan S. Ben-Shachar <35330040+mattansb@users.noreply.github.com>
2020-04-16 17:28:38 +0300

    Update print.R
    https://github.com/easystats/easystats/issues/65

--- R/print.R
+++ R/print.R
@@ -2,10 +2,12 @@
 #' @importFrom parameters parameters_table
 #' @keywords internal
 .print_estimate <- function(x, ...) {
+  orig_x < - x
   if ("Size" %in% names(x)) x$Size <- ifelse(x$Size < 1, paste0(insight::format_value(x$Size * 100), "%"), "100%")
   if ("Part" %in% names(x)) x$Part <- insight::format_value(x$Part, protect_integers = TRUE)
   formatted_table <- parameters::parameters_table(x, ...)
   cat(insight::format_table(formatted_table))
+  invisible(orig_x) 
 }
 
 #' @export
