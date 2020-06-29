1f7cc8ca5f222d92e72d61087cd6f850c859889c
Daniel <mail@danielluedecke.de>
2020-06-29 08:36:51 +0200

    fix test

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -7,58 +7,48 @@ if (require("testthat") && require("modelbased") && require("glmmTMB") && requir
     data = data
   )
 
+  model2 <- glmmTMB::glmmTMB(
+    count ~ cover + mined + (1 | site),
+    ziformula = ~ cover + mined,
+    family = glmmTMB::truncated_poisson,
+    data = data
+  )
+
   test_that("estimate_means - glmmTMB", {
     estim <- estimate_means(model)
     estim2 <- as.data.frame(emmeans::emmeans(model, ~mined, type = "response"))
-    testthat::expect_equal(estim$rate, rev(estim2$rate), tolerance = 1e-3)
+    testthat::expect_equal(estim$rate, estim2$rate, tolerance = 1e-3)
 
     estim <- estimate_means(model, component = "zi")
     estim2 <- as.data.frame(emmeans::emmeans(model, ~mined, component = "zi", type = "response"))
-    testthat::expect_equal(estim$rate, rev(estim2$rate), tolerance = 1e-3)
+    testthat::expect_equal(estim$rate, estim2$rate, tolerance = 1e-3)
   })
 
   test_that("estimate_contrasts - glmmTMB", {
     estim <- estimate_contrasts(model)
     testthat::expect_equal(c(nrow(estim), ncol(estim)), c(1, 10))
+    testthat::expect_equal(estim$Difference, -1.141923, tolerance = 1e-1)
+    testthat::expect_equal(c(estim$Level1[1], estim$Level2[1]), c("yes", "no"))
 
     estim <- estimate_contrasts(model, component = "zi")
     testthat::expect_equal(c(nrow(estim), ncol(estim)), c(1, 10))
+    testthat::expect_equal(estim$Difference, 1.736067, tolerance = 1e-1)
+    testthat::expect_equal(c(estim$Level1[1], estim$Level2[1]), c("yes", "no"))
   })
 
   test_that("estimate_slope - glmmTMB", {
-    model <- glmmTMB::glmmTMB(
-      count ~ cover + mined + (1 | site),
-      ziformula = ~ cover + mined,
-      family = glmmTMB::truncated_poisson,
-      data = data
-    )
-
-    estim <- estimate_slopes(model, trend = "cover")
-    estim2 <- as.data.frame(emmeans::emtrends(model, "mined", var = "cover", transform = "response"))
-    testthat::expect_equal(estim$Coefficient, estim2$cover.trend, tolerance = 1e-3)
+    estim <- estimate_slopes(model2, trend = "cover")
+    estim2 <- as.data.frame(emmeans::emtrends(model2, "mined", var = "cover", transform = "response"))
+    testthat::expect_equal(estim$Coefficient, estim2$cover.trend, tolerance = 1e-2)
   })
 
   test_that("estimate_response - glmmTMB", {
-    model <- glmmTMB::glmmTMB(
-      count ~ cover + (1 | site),
-      ziformula = ~ cover + mined,
-      family = glmmTMB::truncated_poisson,
-      data = data
-    )
-
-    estim <- estimate_response(model)
+    estim <- estimate_response(model2)
     testthat::expect_equal(c(nrow(estim), ncol(estim)), c(nrow(data), 6))
   })
 
   test_that("estimate_link - glmmTMB", {
-    model <- glmmTMB::glmmTMB(
-      count ~ cover + (1 | site),
-      ziformula = ~ cover + mined,
-      family = glmmTMB::truncated_poisson,
-      data = data
-    )
-
-    # estim <- estimate_link(model)
-    # testthat::expect_equal(c(nrow(estim), ncol(estim)), c(nrow(data), 6))
+    estim <- estimate_link(model)
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(35, 6))
   })
 }
