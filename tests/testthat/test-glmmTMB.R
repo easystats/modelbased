c7c49bc51190f9c3e606ad0f6261e18c8f5a8a53
Daniel <mail@danielluedecke.de>
2020-06-29 08:59:12 +0200

    typo

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -48,7 +48,7 @@ if (require("testthat") && require("modelbased") && require("glmmTMB") && requir
   })
 
   test_that("estimate_link - glmmTMB", {
-    estim <- estimate_link(model)
+    estim <- estimate_link(model2)
     testthat::expect_equal(c(nrow(estim), ncol(estim)), c(35, 6))
   })
 }
