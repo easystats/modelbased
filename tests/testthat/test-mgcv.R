e9d56c5eec820ca00a30822f90a1b7152e085f59
DominiqueMakowski <dom.mak19@gmail.com>
2020-08-05 23:15:43 +0800

    Create test-mgcv.R

---
+++ tests/testthat/test-mgcv.R
@@ -0,0 +1,25 @@
+if (require("testthat") && require("modelbased") && require("mgcv") && require("emmeans")) {
+
+  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by=Species), data = iris)
+
+
+  test_that("estimate_means - mgcv", {
+    estim <- estimate_means(model)
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(3, 5))
+  })
+
+  test_that("estimate_contrasts - mgcv", {
+    estim <- estimate_contrasts(model)
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(3, 10))
+  })
+
+  test_that("estimate_response - mgcv", {
+    estim <- estimate_response(model)
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(150, 5))
+  })
+
+  test_that("estimate_link - mgcv", {
+    estim <- estimate_link(model)
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(52, 5))
+  })
+}
