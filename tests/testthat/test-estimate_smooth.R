c2ffcceb9a3e75caa0d51c5ef235db400a99613e
Daniel <mail@danielluedecke.de>
2020-09-27 14:07:37 +0200

    fix test

--- tests/testthat/test-estimate_smooth.R
+++ tests/testthat/test-estimate_smooth.R
@@ -5,7 +5,7 @@ if (require("testthat") && require("modelbased") && require("rstanarm")) {
 
     model <- rstanarm::stan_gamm4(Sepal.Width ~ s(Petal.Length), data = iris, refresh = 0, iter = 200, chains = 2, seed = 333)
     estim <- estimate_smooth(model)
-    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(2, 7))
+    testthat::expect_equal(c(nrow(estim), ncol(estim)), c(7, 6))
 
     model <- rstanarm::stan_glm(Sepal.Width ~ poly(Petal.Length, 2), data = iris, refresh = 0, iter = 200, chains = 2, seed = 333)
     estim <- estimate_smooth(model)
