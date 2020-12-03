ad19f716cd6be50e540fb62733559b248522e64c
Daniel <mail@danielluedecke.de>
2020-12-03 08:28:27 +0100

    suppress warnings

--- tests/testthat/test-estimate_response.R
+++ tests/testthat/test-estimate_response.R
@@ -1,25 +1,25 @@
 if (require("testthat") && require("modelbased") && require("rstanarm") && require("MASS") && require("insight") && require("brms") && require("lme4")) {
   if (Sys.info()["sysname"] != "Darwin") {
     test_that("estimate_response - Bayesian", {
-      model <- rstanarm::stan_glm(mpg ~ wt + poly(cyl, 2, raw = TRUE), data = mtcars, refresh = 0, iter = 200, chains = 2)
+      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt + poly(cyl, 2, raw = TRUE), data = mtcars, refresh = 0, iter = 200, chains = 2))
       estim <- estimate_response(model, seed = 333)
       testthat::expect_equal(nrow(estim), nrow(mtcars))
 
-      model <- rstanarm::stan_glm(mpg ~ wt * as.factor(gear), data = mtcars, refresh = 0, iter = 200, chains = 2)
+      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt * as.factor(gear), data = mtcars, refresh = 0, iter = 200, chains = 2))
       estim <- estimate_response(model, data = "grid", seed = 333)
       testthat::expect_equal(c(nrow(estim), ncol(estim)), c(43, 5))
 
-      model <- rstanarm::stan_glm(mpg ~ as.factor(gear) / wt, data = mtcars, refresh = 0, iter = 200, chains = 2)
+      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ as.factor(gear) / wt, data = mtcars, refresh = 0, iter = 200, chains = 2))
       estim <- estimate_response(model)
       testthat::expect_equal(c(nrow(estim), ncol(estim)), c(32, 5))
 
-      model <- rstanarm::stan_glm(Sepal.Width ~ Petal.Width, data = iris, refresh = 0, iter = 200, chains = 2)
+      model <- suppressWarnings(rstanarm::stan_glm(Sepal.Width ~ Petal.Width, data = iris, refresh = 0, iter = 200, chains = 2))
       estim <- estimate_link(model, keep_draws = TRUE)
       draws <- reshape_draws(estim)
       testthat::expect_equal(c(nrow(draws), ncol(draws)), c(5000, 7))
 
       # Polr
-      model <- rstanarm::stan_polr(Species ~ Petal.Width + Petal.Length, data = iris, refresh = 0, iter = 200, chains = 2, prior = rstanarm::R2(0.2, "mean"))
+      model <- suppressWarnings(rstanarm::stan_polr(Species ~ Petal.Width + Petal.Length, data = iris, refresh = 0, iter = 200, chains = 2, prior = rstanarm::R2(0.2, "mean")))
       estim <- estimate_link(model, length = 5)
       testthat::expect_equal(c(nrow(estim), ncol(estim)), c(25, 5))
 
