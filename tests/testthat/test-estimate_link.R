d3e3cbaac266c86388298532932f9b451d59c885
Daniel <mail@danielluedecke.de>
2021-01-25 12:02:54 +0100

    Update test-estimate_link.R

--- tests/testthat/test-estimate_link.R
+++ tests/testthat/test-estimate_link.R
@@ -1,3 +1,13 @@
+osx <- tryCatch({
+  si <- Sys.info()
+  if (!is.null(si["sysname"])) {
+    si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+  } else {
+    FALSE
+  }
+})
+
+
 if (require("testthat") && require("modelbased") && require("gamm4") && require("rstanarm") && require("lme4") && require("glmmTMB") && require("mgcv")) {
   test_that("estimate_link", {
 
@@ -26,12 +36,14 @@ if (require("testthat") && require("modelbased") && require("gamm4") && require(
     testthat::expect_equal(dim(modelbased::estimate_link(model, include_smooth = FALSE, length = 3)), c(3, 4))
 
 
-    # STAN_GAMM4
-    model <- suppressWarnings(rstanarm::stan_gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length),
-      random = ~ (1 | Species), data = iris,
-      iter = 100, chains = 2, refresh = 0
-    ))
-    testthat::expect_equal(nrow(modelbased::estimate_link(model, length = 3)), 9)
-    testthat::expect_equal(dim(modelbased::estimate_link(model, include_smooth = FALSE, length = 3)), c(3, 4))
+    if (!osx) {
+      # STAN_GAMM4
+      model <- suppressWarnings(rstanarm::stan_gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length),
+                                                     random = ~ (1 | Species), data = iris,
+                                                     iter = 100, chains = 2, refresh = 0
+      ))
+      testthat::expect_equal(nrow(modelbased::estimate_link(model, length = 3)), 9)
+      testthat::expect_equal(dim(modelbased::estimate_link(model, include_smooth = FALSE, length = 3)), c(3, 4))
+    }
   })
 }
