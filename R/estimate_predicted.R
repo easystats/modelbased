8972b8339d9240afc4808f353bbe4ba78ba12e02
DominiqueMakowski <dom.mak19@gmail.com>
2021-03-11 08:42:14 +0800

    init

--- R/estimate_predicted.R
+++ R/estimate_predicted.R
@@ -1,4 +1,4 @@
-#' Generates predictions for models
+#' Generates predictions from models
 #'
 #' \code{estimate_link} is a shortcut to \code{estimate_response} with \code{data = "grid"}. \code{estimate_response} would be used in the context of generating actual predictions for the existing or new data, whereas \code{estimate_link} is more relevant in the context of visualisation and plotting. There are many control parameters that are not listed here but can be used, such as the arguments from  \code{\link{visualisation_matrix}} (used when \code{data = "grid"}) and from \code{\link[insight:get_predicted]{insight::get_predicted()}} (the function to compute predictions used internally).
 #'
@@ -14,29 +14,56 @@
 #' # Linear Models
 #' model <- lm(mpg ~ wt, data = mtcars)
 #' estimate_response(model)
-#' estimate_link(model)
+#' estimate_relation(model)
 #'
 #' # Logistic Models
 #' model <- glm(vs ~ wt, data = mtcars, family = "binomial")
 #' estimate_response(model)
-#' estimate_link(model)
+#' estimate_relation(model)
 #'
 #' # Mixed models
 #' if (require("lme4")) {
 #'   model <- lmer(mpg ~ wt + (1 | gear), data = mtcars)
 #'   estimate_response(model)
-#'   estimate_link(model)
+#'   estimate_relation(model)
 #' }
 #'
 #' # Bayesian models
 #' if (require("rstanarm")) {
 #'   model <- rstanarm::stan_glm(mpg ~ wt, data = mtcars, refresh=0)
 #'   estimate_response(model)
-#'   estimate_link(model)
+#'   estimate_relation(model)
 #' }
 #' @return A dataframe of predicted values.
 #' @export
-estimate_response <- function(model, data = NULL, predict = "response", ci = 0.95, keep_iterations = FALSE, ...) {
+estimate_relation <- function(model, data = "grid", ci = 0.95, keep_iterations = FALSE, ...) {
+  .estimate_predicted(model, data = data, ci = ci, keep_iterations = keep_iterations, predict = "relation", ...)
+}
+
+#' @rdname estimate_relation
+#' @export
+estimate_link <- function(model, data = "grid", predict = "link", ci = 0.95, keep_iterations = FALSE, ...) {
+  .estimate_predicted(model, data = data, ci = ci, keep_iterations = keep_iterations, predict = "link", ...)
+}
+
+
+#' @rdname estimate_relation
+#' @export
+estimate_prediction <- function(model, data = NULL, ci = 0.95, keep_iterations = FALSE, ...) {
+  .estimate_predicted(model, data = data, ci = ci, keep_iterations = keep_iterations, predict = "prediction", ...)
+}
+
+#' @rdname estimate_relation
+#' @export
+estimate_response <- estimate_prediction
+
+
+
+
+# Internal ----------------------------------------------------------------
+
+#' @keywords internal
+.estimate_predicted <- function(model, data = "grid", predict = "relation", ci = 0.95, keep_iterations = FALSE, ...) {
 
   # Get data ----------------
   if (is.null(data)) {
@@ -54,26 +81,14 @@ estimate_response <- function(model, data = NULL, predict = "response", ci = 0.9
   data <- data_restoretype(data, insight::get_data(model))
 
   # Get predicted ----------------
-  ci_type <- ifelse(predict == "link", "confidence", "prediction")
-  predicted <- insight::get_predicted(model, newdata = data, ci = ci, ci_type = ci_type, ...)
-
-  # Format predicted ----------------
-  if(insight::model_info(model)$is_bayesian) {
-    out <- bayestestR::describe_posterior(predicted, ci = ci, ...)
-    centrality <- c("Median", "Mean", "MAP")[c("Median", "Mean", "MAP") %in% names(out)][1]
-    out$Predicted <- out[[centrality]]
-  } else {
-    out <- as.data.frame(predicted)
-    centrality <- NULL
-  }
-
-  # Select columns
-  out <- out[c("Predicted", "CI_low", "CI_high")[c("Predicted", "CI_low", "CI_high") %in% names(out)]]
-  if(keep_iterations && "iter_1" %in% names(predicted)) out <- cbind(out, predicted)
-
-  # Bind data and predicted ----------------
-  out <- cbind(data, out)
-  row.names(out) <- NULL
+  predictions <- insight::get_predicted(model,
+                                        newdata = data,
+                                        predict = predict,
+                                        ci = ci,
+                                        dispersion_function = "mad",
+                                        interval_function = "hdi",
+                                        ...)
+  out <- as.data.frame(predictions, keep_iterations = keep_iterations)
 
   # Prepare output
   attr(out, "ci") <- ci
@@ -82,14 +97,4 @@ estimate_response <- function(model, data = NULL, predict = "response", ci = 0.9
   attr(out, "response") <- insight::find_response(model)
   class(out) <- c("estimate_response", "see_estimate_response", class(out))
   out
-}
-
-
-
-
-
-#' @rdname estimate_response
-#' @export
-estimate_link <- function(model, data = "grid", predict = "link", ci = 0.95, keep_iterations = FALSE, ...) {
-  estimate_response(model, data = data, predict = predict, ci = ci, keep_iterations = keep_iterations, ...)
 }
