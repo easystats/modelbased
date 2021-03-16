fc8d055db3cf6e82a6a55cc030d49e4aca35771f
Indrajeet Patil <inderonline1988@gmail.com>
2021-03-16 10:06:06 +0100

    minor edits to data grids vignette; spell check

--- tests/testthat/test-estimate_predicted.R
+++ tests/testthat/test-estimate_predicted.R
@@ -8,8 +8,6 @@ osx <- tryCatch({
 })
 
 if (require("testthat") && require("modelbased") && require("gamm4") && require("rstanarm") && require("lme4") && require("glmmTMB") && require("mgcv") && require("MASS") && require("brms") && require("testthat")) {
-
-
   test_that("estimate_link", {
 
     # LMER4
@@ -31,7 +29,7 @@ if (require("testthat") && require("modelbased") && require("gamm4") && require(
 
     # GAMM4
     model <- gamm4::gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length),
-                          random = ~ (1 | Species), data = iris
+      random = ~ (1 | Species), data = iris
     )
     expect_equal(nrow(modelbased::estimate_link(model, length = 3)), 9)
     expect_equal(dim(modelbased::estimate_link(model, include_smooth = FALSE, length = 3)), c(3, 5))
@@ -40,8 +38,8 @@ if (require("testthat") && require("modelbased") && require("gamm4") && require(
     if (!osx) {
       # STAN_GAMM4
       model <- suppressWarnings(rstanarm::stan_gamm4(Petal.Length ~ Petal.Width + s(Sepal.Length),
-                                                     random = ~ (1 | Species), data = iris,
-                                                     iter = 100, chains = 2, refresh = 0
+        random = ~ (1 | Species), data = iris,
+        iter = 100, chains = 2, refresh = 0
       ))
       expect_equal(nrow(modelbased::estimate_relation(model, length = 3)), 9)
       expect_equal(dim(modelbased::estimate_link(model, include_smooth = FALSE, length = 3)), c(3, 5))
@@ -52,38 +50,38 @@ if (require("testthat") && require("modelbased") && require("gamm4") && require(
 
 
 
-    test_that("estimate_response - Bayesian", {
-      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt + poly(cyl, 2, raw = TRUE), data = mtcars, refresh = 0, iter = 200, chains = 2))
-      estim <- estimate_response(model, seed = 333)
-      expect_equal(nrow(estim), nrow(mtcars))
+  test_that("estimate_response - Bayesian", {
+    model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt + poly(cyl, 2, raw = TRUE), data = mtcars, refresh = 0, iter = 200, chains = 2))
+    estim <- estimate_response(model, seed = 333)
+    expect_equal(nrow(estim), nrow(mtcars))
 
-      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt * as.factor(gear), data = mtcars, refresh = 0, iter = 200, chains = 2))
-      estim <- estimate_response(model, data = "grid", seed = 333)
-      expect_equal(c(nrow(estim), ncol(estim)), c(30, 6))
+    model <- suppressWarnings(rstanarm::stan_glm(mpg ~ wt * as.factor(gear), data = mtcars, refresh = 0, iter = 200, chains = 2))
+    estim <- estimate_response(model, data = "grid", seed = 333)
+    expect_equal(c(nrow(estim), ncol(estim)), c(30, 6))
 
-      model <- suppressWarnings(rstanarm::stan_glm(mpg ~ as.factor(gear) / wt, data = mtcars, refresh = 0, iter = 200, chains = 2))
-      estim <- estimate_response(model)
-      expect_equal(c(nrow(estim), ncol(estim)), c(32, 6))
+    model <- suppressWarnings(rstanarm::stan_glm(mpg ~ as.factor(gear) / wt, data = mtcars, refresh = 0, iter = 200, chains = 2))
+    estim <- estimate_response(model)
+    expect_equal(c(nrow(estim), ncol(estim)), c(32, 6))
 
-      model <- suppressWarnings(rstanarm::stan_glm(Sepal.Width ~ Petal.Width, data = iris, refresh = 0, iter = 200, chains = 2))
-      estim <- estimate_link(model, keep_iterations = TRUE)
-      draws <- bayestestR::reshape_iterations(estim)
-      expect_equal(c(nrow(draws), ncol(draws)), c(2000, 8))
+    model <- suppressWarnings(rstanarm::stan_glm(Sepal.Width ~ Petal.Width, data = iris, refresh = 0, iter = 200, chains = 2))
+    estim <- estimate_link(model, keep_iterations = TRUE)
+    draws <- bayestestR::reshape_iterations(estim)
+    expect_equal(c(nrow(draws), ncol(draws)), c(2000, 8))
 
-      # Polr
-      model <- suppressWarnings(rstanarm::stan_polr(Species ~ Petal.Width + Petal.Length, data = iris, refresh = 0, iter = 200, chains = 2, prior = rstanarm::R2(0.2, "mean")))
-      estim <- estimate_link(model, length = 6)
-      expect_equal(c(nrow(estim), ncol(estim)), c(36, 6))
+    # Polr
+    model <- suppressWarnings(rstanarm::stan_polr(Species ~ Petal.Width + Petal.Length, data = iris, refresh = 0, iter = 200, chains = 2, prior = rstanarm::R2(0.2, "mean")))
+    estim <- estimate_link(model, length = 6)
+    expect_equal(c(nrow(estim), ncol(estim)), c(36, 6))
 
-      # Non-sampling algorithms
-      model <- rstanarm::stan_glm(mpg ~ disp, data = mtcars, algorithm = "meanfield", refresh = 0)
-      estim <- estimate_link(model, keep_iterations = TRUE)
-      expect_equal(c(nrow(estim), ncol(estim)), c(10, 1005))
+    # Non-sampling algorithms
+    model <- rstanarm::stan_glm(mpg ~ disp, data = mtcars, algorithm = "meanfield", refresh = 0)
+    estim <- estimate_link(model, keep_iterations = TRUE)
+    expect_equal(c(nrow(estim), ncol(estim)), c(10, 1005))
 
-      # model <- brms::brm(mpg ~ drat, data = mtcars, algorithm = "meanfield", refresh=0)
-      # estim <- estimate_link(model, keep_iterations = TRUE)
-      # expect_equal(c(nrow(estim), ncol(estim)), c(25, 1004))
-    })
+    # model <- brms::brm(mpg ~ drat, data = mtcars, algorithm = "meanfield", refresh=0)
+    # estim <- estimate_link(model, keep_iterations = TRUE)
+    # expect_equal(c(nrow(estim), ncol(estim)), c(25, 1004))
+  })
 
 
   test_that("estimate_response - Frequentist", {
@@ -119,5 +117,4 @@ if (require("testthat") && require("modelbased") && require("gamm4") && require(
     # # TODO: why no CI?
     # expect_equal(c(nrow(estim), ncol(estim)), c(10, 1))
   })
-
 }
