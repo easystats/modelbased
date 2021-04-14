7a4d60aacba4bb5bf498f5f097102017e866266b
Daniel <mail@danielluedecke.de>
2021-04-14 10:26:19 +0200

    don't run on old osx

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,20 @@
 library(testthat)
 library(modelbased)
 
-test_check("modelbased")
+osx <- tryCatch(
+  {
+    si <- Sys.info()
+    if (!is.null(si["sysname"])) {
+      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+    } else {
+      FALSE
+    }
+  },
+  error = function(e) {
+    FALSE
+  }
+)
+
+if (!osx || (osx && getRversion() >= "4.0.0")) {
+  test_check("modelbased")
+}
