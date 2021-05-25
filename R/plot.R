8a3d20392b2b708a02d3a4122121b629fc5c943f
Dominique Makowski <dom.mak19@gmail.com>
2021-05-25 10:35:49 +0800

    website order

--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,5 @@
 #' @export
 plot.estimate_contrasts <- function(x, ...) {
-  if (!requireNamespace("see", quietly = TRUE)) {
-    stop("Package 'see' needed to plot results from contrast analysis. Please install it by running `install.packages('see')`.")
-  }
+  insight::check_if_installed("see")
   NextMethod()
 }
