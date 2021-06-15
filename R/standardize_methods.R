easystats/modelbased

commit d3785239fa4b262b7a9c2f23c2dbad45bccfdf80

remove imports

NAMESPACE
@@ -77,22 +77,3 @@ export(smoothing)
 export(visualisation_matrix)
 export(visualisation_recipe)
 export(zero_crossings)
-importFrom(effectsize,standardize)
-importFrom(effectsize,unstandardize)
-importFrom(insight,export_table)
-importFrom(insight,find_predictors)
-importFrom(insight,find_random)
-importFrom(insight,get_data)
-importFrom(stats,approxfun)
-importFrom(stats,lm)
-importFrom(stats,loess)
-importFrom(stats,mad)
-importFrom(stats,median)
-importFrom(stats,na.omit)
-importFrom(stats,predict)
-importFrom(stats,quantile)
-importFrom(stats,sd)
-importFrom(stats,setNames)
-importFrom(stats,smooth)
-importFrom(stats,uniroot)
-importFrom(utils,tail)

R/standardize_methods.R
@@ -3,7 +3,6 @@
 
 
 
-#' @importFrom effectsize standardize
 #' @export
 standardize.visualisation_matrix <- function(x, ...) {
   x[names(x)] <- effectsize::standardize(as.data.frame(x), reference = attributes(x)$data, ...)
@@ -55,7 +54,6 @@ standardize.estimate_means <- standardize.estimate_predicted
 
 
 
-#' @importFrom stats sd mad
 #' @export
 standardize.estimate_contrasts <- function(x, robust = FALSE, ...) {
   model <- attributes(x)$model
@@ -85,7 +83,6 @@ standardize.estimate_slopes <- standardize.estimate_contrasts
 # Unstandardize -------------------------------------------------------------
 
 #' @method unstandardize visualisation_matrix
-#' @importFrom effectsize unstandardize
 #' @export
 unstandardize.visualisation_matrix <- function(x, ...) {
   x[names(x)] <- effectsize::unstandardize(as.data.frame(x), reference = attributes(x)$data, ...)

R/describe_nonlinear.R
@@ -23,8 +23,6 @@
 #'
 #' describe_nonlinear(link_data, x = "x")
 #' @return A dataframe of linear description of non-linear terms.
-#' @importFrom insight find_predictors get_data find_random
-#' @importFrom stats mad median sd setNames predict loess
 #' @export
 describe_nonlinear <- function(data, ...) {
   UseMethod("describe_nonlinear")
@@ -136,7 +134,6 @@ estimate_smooth <- describe_nonlinear
 
 
 
-#' @importFrom stats lm
 #' @keywords internal
 .check_linearity <- function(y, x) {
   performance::r2(stats::lm(y ~ x))$R2

R/model_emmeans.R
@@ -118,7 +118,6 @@ model_emmeans <- function(model,
 # =========================================================================
 
 
-#' @importFrom insight find_predictors get_data
 #' @keywords internal
 .guess_emmeans_arguments <- function(model, levels = NULL, fixed = NULL, modulate = NULL) {
   x <- .guess_emmeans_levels(model, levels = levels)
@@ -145,7 +144,6 @@ model_emmeans <- function(model,
   )
 }
 
-#' @importFrom utils tail
 #' @keywords internal
 .guess_emmeans_levels <- function(model, levels = NULL) {
 

R/print.R
@@ -1,4 +1,3 @@
-#' @importFrom insight export_table
 #' @export
 print.estimate_contrasts <- function(x, ...) {
   cat(insight::export_table(format(x), ...))

R/smoothing.R
@@ -22,7 +22,6 @@
 #' plot(x, type = "l")
 #' lines(smoothing(x, method = "smooth"), type = "l", col = "blue")
 #' lines(smoothing(x, method = "loess"), type = "l", col = "red")
-#' @importFrom stats predict loess smooth
 #' @return A smoothed vector or data frame.
 #' @export
 smoothing <- function(x, method = "loess", strength = 0.25, ...) {
@@ -30,7 +29,6 @@ smoothing <- function(x, method = "loess", strength = 0.25, ...) {
 }
 
 
-#' @importFrom stats predict loess smooth
 #' @export
 smoothing.numeric <- function(x, method = "loess", strength = 0.25, ...) {
   if (strength == 0 | strength == FALSE | is.null(method)) {

R/visualisation_matrix.data.frame.R
@@ -39,7 +39,6 @@
 #' # Standardize
 #' vizdata <- visualisation_matrix(data, target = "Sepal.Length")
 #' effectsize::standardize(vizdata)
-#' @importFrom stats na.omit
 #' @export
 visualisation_matrix <- function(x, ...) {
   UseMethod("visualisation_matrix")
@@ -160,7 +159,6 @@ visualisation_matrix.data.frame <- function(x, target = "all", factors = "refere
 
 # Utils -------------------------------------------------------------------
 
-#' @importFrom stats na.omit
 #' @keywords internal
 .visualisation_matrix_summary <- function(x, numerics = "mean", factors = "reference", na.rm = TRUE, ...) {
   if (na.rm == TRUE) x <- stats::na.omit(x)
@@ -275,14 +273,13 @@ visualisation_matrix.logical <- visualisation_matrix.character
 
 
 
-#' @importFrom stats quantile
 #' @keywords internal
 .create_spread <- function(x, length = 10, range = "range", ci = 0.95, ...) {
   range <- match.arg(tolower(range), c("range", "iqr", "ci", "hdi", "eti"))
 
   if (range == "iqr") {
-    mini <- quantile(x, (1 - ci) / 2, ...)
-    maxi <- quantile(x, (1 + ci) / 2, ...)
+    mini <- stats::quantile(x, (1 - ci) / 2, ...)
+    maxi <- stats::quantile(x, (1 + ci) / 2, ...)
   } else if (range == "ci") {
     out <- bayestestR::ci(x, ci = ci, ...)
     mini <- out$CI_low

R/zero_crossings.R
@@ -9,7 +9,6 @@
 #' x <- sin(seq(0, 4 * pi, length.out = 100))
 #' plot(x)
 #' zero_crossings(x)
-#' @importFrom stats approxfun
 #' @return Vector of zero crossings.
 #' @seealso Based on the \code{uniroot.all} function from the rootSolve package.
 #' @export
@@ -29,7 +28,6 @@ zero_crossings <- function(x) {
 
 
 #' Copied from rootSolve package
-#' @importFrom stats uniroot
 #' @keywords internal
 .uniroot.all <- function(f,
                          interval,

commit ba86e538016d1bf52442542d3a73ce0ae7525008

fix issue
it got broken in https://github.com/easystats/modelbased/commit/d3785239fa4b262b7a9c2f23c2dbad45bccfdf80

NAMESPACE
@@ -77,3 +77,5 @@ export(smoothing)
 export(visualisation_matrix)
 export(visualisation_recipe)
 export(zero_crossings)
+importFrom(effectsize,standardize)
+importFrom(effectsize,unstandardize)

R/standardize_methods.R
@@ -2,7 +2,8 @@
 
 
 
-
+#' @importFrom effectsize standardize
+#' @method standardize visualisation_matrix
 #' @export
 standardize.visualisation_matrix <- function(x, ...) {
   x[names(x)] <- effectsize::standardize(as.data.frame(x), reference = attributes(x)$data, ...)
@@ -83,6 +84,7 @@ standardize.estimate_slopes <- standardize.estimate_contrasts
 # Unstandardize -------------------------------------------------------------
 
 #' @method unstandardize visualisation_matrix
+#' @importFrom effectsize unstandardize
 #' @export
 unstandardize.visualisation_matrix <- function(x, ...) {
   x[names(x)] <- effectsize::unstandardize(as.data.frame(x), reference = attributes(x)$data, ...)
@@ -92,6 +94,7 @@ unstandardize.visualisation_matrix <- function(x, ...) {
 
 
 #' @method unstandardize estimate_predicted
+#' @importFrom effectsize unstandardize
 #' @export
 unstandardize.estimate_predicted <- function(x, include_response = TRUE, ...) {
   # Get data of predictors