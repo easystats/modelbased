c9f54258d79efd56253d6ceab70f3428f7dacf32
Dominique Makowski <dom.mak19@gmail.com>
2022-03-01 18:25:53 +0800

    data_matrix alias is removed

--- R/visualisation_matrix.R
+++ R/visualisation_matrix.R
@@ -1,17 +1,9 @@
 #' Create a reference grid
 #'
-#' Create a reference matrix, useful for visualisation, with evenly spread and combined values. `data_matrix()` is an alternative name for `visualisation_matrix()`.
+#' See `insight::get_datagrid()`.
 #'
-#' @param x An object from which to construct the reference grid.
-#' @param at,target Can be "all" or list of characters indicating columns of interest. Can also contain assignments (as named list, e.g. `at = list(c(Sepal.Length = c(2, 4), Species = "setosa"))`, or as string, e.g. `at = "Sepal.Length = 2"` or `at = c("Sepal.Length = 2", "Species = 'setosa'")` - note the usage of single and double quotes to assign strings within strings). The remaining variables will be fixed. (`target` is the deprecated name of that argument).
-#' @param length Length of numeric "at" variables.
-#' @param range Can be one of `c("range", "iqr", "ci", "hdi", "eti")`. If `"range"` (default), will use the min and max of the original vector as end-points. If any other interval, will spread within the range (the default CI width is `95%` but this can be changed by setting something else, e.g., `ci = 0.90`). See [IQR()] and [bayestestR::ci()].
-#' @param factors Type of summary for factors. Can be "reference" (set at the reference level), "mode" (set at the most common level) or "all" to keep all levels.
-#' @param numerics Type of summary for numeric values. Can be "all" (will duplicate the grid for all unique values), any function ("mean", "median", ...) or a value (e.g., `numerics = 0`).
-#' @param preserve_range In the case of combinations between numeric variables and factors, setting `preserve_range = TRUE` will drop the observations where the value of the numeric variable is originally not present in the range of its factor level. This leads to an unbalanced grid. Also, if you want the minimum and the maximum to closely match the actual ranges, you should increase the `length` argument.
-#' @param ... Arguments passed to or from other methods (for instance, `length` or `range` to control the spread of numeric variables.).
-#' @inheritParams effectsize::format_standardize
-#' @inheritParams estimate_response
+#' @inheritParams insight::get_datagrid
+#' @param target Deprecated name. Please use `at` instead.
 #'
 #'
 #' @return Reference grid data frame.
@@ -47,11 +39,6 @@ visualisation_matrix <- function(x, ...) {
   UseMethod("visualisation_matrix")
 }
 
-#' @rdname visualisation_matrix
-#' @export
-data_matrix <- visualisation_matrix
-
-
 # -------------------------------------------------------------------------
 # Below are visualisation_matrix functions for DataFrames
 # -------------------------------------------------------------------------
@@ -75,8 +62,8 @@ visualisation_matrix.data.frame <- function(x, at = "all", target = NULL, factor
 
 #' @rdname visualisation_matrix
 #' @export
-visualisation_matrix.numeric <- function(x, length = 10, range = "range", ...) {
-  insight::get_datagrid(x, length = length, range = range, ...)
+visualisation_matrix.numeric <- function(x, ...) {
+  insight::get_datagrid(x, ...)
 }
 
 #' @export
@@ -84,14 +71,23 @@ visualisation_matrix.double <- visualisation_matrix.numeric
 
 #' @rdname visualisation_matrix
 #' @export
-visualisation_matrix.factor <- function(x, ...) {
-  insight::get_datagrid(x, ...)
-}
+visualisation_matrix.factor <- visualisation_matrix.numeric
 
 #' @export
 visualisation_matrix.character <- visualisation_matrix.factor
 
 #' @export
 visualisation_matrix.logical <- visualisation_matrix.character
+
+# -------------------------------------------------------------------------
+# Below are visualisation_matrix functions that work on statistical models
+# -------------------------------------------------------------------------
+
+#' @export
+visualisation_matrix.default <- visualisation_matrix.numeric
+
+
+#' @export
+visualisation_matrix.visualisation_matrix <- visualisation_matrix.numeric
 
 
