5c1b11d3addc131e08f3becc5dd5a39000625251
Daniel <mail@danielluedecke.de>
2022-05-19 10:23:10 +0200

    Update test-attributes_visualisation.R

--- tests/testthat/test-attributes_visualisation.R
+++ tests/testthat/test-attributes_visualisation.R
@@ -1,4 +1,4 @@
-if (require("testthat") && require("modelbased")) {
+if (require("testthat") && require("modelbased") && require("emmeans")) {
   test_that("attributes_means", {
     model <- lm(Sepal.Length ~ Species * Sepal.Width, data = iris)
 
