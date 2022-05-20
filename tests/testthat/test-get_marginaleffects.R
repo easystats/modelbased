ce043691df5c89836c4d37d4ba38b81406b1f48c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-05-20 22:52:55 +0200

    relax the test

--- tests/testthat/test-get_marginaleffects.R
+++ tests/testthat/test-get_marginaleffects.R
@@ -2,7 +2,7 @@ if (require("testthat") && require("modelbased") && require("marginaleffects") &
   test_that("get_marginaleffects", {
     model <- lm(Sepal.Width ~ Species * Petal.Length, data = iris)
 
-    expect_equal(dim(get_marginaleffects(model, trend = "Petal.Length", at = "Species")), c(3, 11))
+    expect_equal(nrow(get_marginaleffects(model, trend = "Petal.Length", at = "Species")), 3L)
 
     # get_marginaleffects(model, trend = "Petal.Length", at = "Species", length = 10)
   })
