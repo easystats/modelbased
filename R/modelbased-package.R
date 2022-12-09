5ade6b4ff46b3cb8a8bba1fcc853476a27617daa
etiennebacher <etienne.bacher@protonmail.com>
2022-12-09 09:54:55 +0100

    add package description file

---
+++ R/modelbased-package.R
@@ -0,0 +1,15 @@
+#' \code{modelbased}
+#'
+#' @title modelbased: Estimation of Model-Based Predictions, Contrasts and Means
+#'
+#' @description
+#'
+#' `modelbased` is a package helping with model-based estimations, to
+#' easily compute of marginal means, contrast analysis and model predictions.
+#'
+#'
+#' @docType package
+#' @aliases modelbased modelbased-package
+#' @name modelbased-package
+#' @keywords internal
+"_PACKAGE"
