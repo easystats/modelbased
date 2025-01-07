3e80b13c48f23fbbc3fe7fdce2560e9febe6bb70
Daniel <mail@danielluedecke.de>
2025-01-07 22:23:32 +0100

    fix test

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -1,5 +1,7 @@
 skip_if_not_installed("glmmTMB")
 skip_if_not_installed("emmeans")
+skip_if_not_installed("marginaleffects")
+skip_if_not_installed("ggeffects")
 
 data <- glmmTMB::Salamanders
 model <- suppressWarnings(glmmTMB::glmmTMB(
@@ -27,10 +29,15 @@ test_that("estimate_means - glmmTMB", {
 })
 
 test_that("estimate_contrasts - glmmTMB", {
-  estim <- suppressMessages(estimate_contrasts(model))
-  expect_identical(dim(estim), c(1L, 9L))
-  expect_equal(estim$Difference, -1.141923, tolerance = 1e-1)
-  expect_identical(c(estim$Level1[1], estim$Level2[1]), c("yes", "no"))
+  estim1 <- suppressMessages(estimate_contrasts(model))
+  pr <- ggeffects::predict_response(model, "mined")
+  estim2 <- ggeffects::test_predictions(pr)
+  expect_identical(dim(estim1), c(1L, 9L))
+  expect_equal(estim1$Difference, -2.32874, tolerance = 1e-3)
+  expect_equal(estim1$Difference, estim2$Contrast, tolerance = 1e-3)
+  expect_identical(c(estim1$Level1[1], estim1$Level2[1]), c("yes", "no"))
+  estim3 <- suppressMessages(estimate_contrasts(model, contrast = "mined", backend = "marginaleffects"))
+  expect_equal(estim3$Difference, -1.99344, tolerance = 1e-3)
 
   estim <- suppressMessages(estimate_contrasts(model, component = "zi"))
   expect_identical(dim(estim), c(1L, 9L))
