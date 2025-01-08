6e8471afb41244e60cabfd933e5a317165990326
Daniel <mail@danielluedecke.de>
2025-01-08 22:42:32 +0100

    get_marginalmeans(..., by=NULL) should return the overall marginal mean Fixes #295

--- tests/testthat/test-get_marginaleffects.R
+++ tests/testthat/test-get_marginaleffects.R
@@ -9,3 +9,14 @@ test_that("get_marginaleffects", {
   out2 <- estimate_slopes(model, trend = "Petal.Length", by = "Species")
   expect_equal(out$estimate, out2$Coefficient, tolerance = 1e-3)
 })
+
+
+test_that("get_marginaleffects, overall mean", {
+  skip_if_not_installed("marginaleffects")
+  skip_if_not_installed("emmeans")
+
+  model <- lm(Sepal.Width ~ Species * Petal.Length, data = iris)
+  out1 <- as.data.frame(get_emmeans(model, by = NULL))
+  out2 <- as.data.frame(get_marginalmeans(model, by = NULL))
+  expect_equal(out1$emmean, out2$estimate, tolerance = 0.2)
+})
