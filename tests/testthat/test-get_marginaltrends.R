1c40ec078afaf2f674f384d77395c55dcb13814a
Daniel <mail@danielluedecke.de>
2025-01-08 19:41:03 +0100

    docs

--- tests/testthat/test-get_marginaltrends.R
+++ tests/testthat/test-get_marginaltrends.R
@@ -5,15 +5,15 @@ test_that("get_marginaltrends", {
   model <- lm(Sepal.Width ~ Species * Petal.Length, data = iris)
 
   # TODO: fix this
-  e1 <- get_emtrends(model, by=NULL, trend = "Petal.Length") |> as.data.frame()
-  e2 <- get_marginaltrends(model, by=NULL, trend = "Petal.Length") |> as.data.frame()
+  e1 <- as.data.frame(get_emtrends(model, by=NULL, trend = "Petal.Length"))
+  e2 <- as.data.frame(get_marginaltrends(model, by=NULL, trend = "Petal.Length"))
   # expect_equal(e1$Petal.Length.trend, e2$estimate)
 
-  e1 <- get_emtrends(model, by="Species", trend = "Petal.Length") |> as.data.frame()
-  e2 <- get_marginaltrends(model, by="Species", trend = "Petal.Length") |> as.data.frame()
+  e1 <- as.data.frame(get_emtrends(model, by = "Species", trend = "Petal.Length"))
+  e2 <- as.data.frame(get_marginaltrends(model, by = "Species", trend = "Petal.Length"))
   expect_equal(e1$Petal.Length.trend, e2$estimate)
 
-  e1 <- get_emtrends(model, by="Petal.Length", trend = "Petal.Length") |> as.data.frame()
-  e2 <- get_marginaltrends(model, by="Petal.Length", trend = "Petal.Length") |> as.data.frame()
+  e1 <- as.data.frame(get_emtrends(model, by = "Petal.Length", trend = "Petal.Length"))
+  e2 <- as.data.frame(get_marginaltrends(model, by = "Petal.Length", trend = "Petal.Length"))
   expect_equal(e1$Petal.Length.trend, e2$estimate)
 })
