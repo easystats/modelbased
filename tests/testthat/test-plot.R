7c9dd354d410725261d649ab29804830b6f9b260
Daniel <mail@danielluedecke.de>
2025-01-09 23:26:06 +0100

    add snapshots

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -29,3 +29,34 @@ test_that("plots", {
     plot(modelbased::visualisation_recipe(x, show_data = FALSE))
   )
 })
+
+
+test_that("plots, show_data", {
+  model <- lm(Sepal.Length ~ Species * Sepal.Width, data = iris)
+
+  # Estimate means -------------------------------------
+  x <- estimate_means(model, by = "Species")
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-means-showdata-1",
+    plot(modelbased::visualisation_recipe(x, show_data = TRUE))
+  )
+  x <- estimate_means(model, by = "Sepal.Width")
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-means-showdata-2",
+    plot(modelbased::visualisation_recipe(x, show_data = TRUE))
+  )
+  x <- estimate_means(model, by = c("Sepal.Width", "Species"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-means-showdata-3",
+    plot(modelbased::visualisation_recipe(x, show_data = TRUE))
+  )
+  x <- estimate_means(model, by = c("Species", "Sepal.Width"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-means-showdata-4",
+    plot(modelbased::visualisation_recipe(x, show_data = TRUE))
+  )
+})
