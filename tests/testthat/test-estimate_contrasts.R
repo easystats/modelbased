b3b384656225c02bfe981e0a89d81ce8514b8d3c
Daniel <mail@danielluedecke.de>
2025-01-09 10:51:54 +0100

    add test

--- tests/testthat/test-estimate_contrasts.R
+++ tests/testthat/test-estimate_contrasts.R
@@ -295,3 +295,19 @@ test_that("estimate_contrasts - on-the-fly factors", {
   expect_identical(nrow(out4), 3L)
   expect_equal(out3$Difference, out4$Difference, tolerance = 1e-4)
 })
+
+
+test_that("estimate_contrasts - works with slopes", {
+  data(iris)
+  fit <- lm(Sepal.Width ~ Petal.Length * Species, data = iris)
+
+  out1 <- estimate_slopes(fit, trend = "Petal.Length", backend = "marginaleffects")
+  out2 <- suppressMessages(as.data.frame(emmeans::emtrends(fit, specs = ~Petal.Length, var = "Petal.Length")))
+  expect_equal(out1$Coefficient, out2$Petal.Length.trend, tolerance = 1e-3)
+
+  out3 <- estimate_slopes(fit, trend = "Petal.Length", by = "Species", backend = "marginaleffects")
+  out4 <- estimate_contrasts(fit, contrast = "Petal.Length", by = "Species", backend = "marginaleffects")
+  out5 <- emmeans::emtrends(fit, specs = pairwise ~ Species, var = "Petal.Length")
+  expect_equal(out3$Coefficient, as.data.frame(out5$emtrends)$Petal.Length.trend, tolerance = 1e-3)
+  expect_equal(out4$Coefficient, as.data.frame(out5$contrasts)$estimate, tolerance = 1e-3)
+})
