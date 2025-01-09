d82622d8b4605f8277fda021d5e391922a2e3177
Dominique Makowski <dom.mak19@gmail.com>
2025-01-09 08:12:19 +0000

    keep SE

--- R/estimate_slopes.R
+++ R/estimate_slopes.R
@@ -173,7 +173,7 @@ estimate_slopes <- function(model,
   # Summarize and clean
   trends <- parameters::parameters(estimated, ci = ci, ...)
   # remove redundant columns
-  trends <- datawizard::data_remove(trends, c("Parameter", "Statistic", "SE", "S", "CI", "df", "rowid_dedup"), verbose = FALSE) # nolint
+  trends <- datawizard::data_remove(trends, c("Parameter", "Statistic", "S", "CI", "df", "rowid_dedup"), verbose = FALSE) # nolint
   trends <- datawizard::data_relocate(trends, "p", after = -1, verbose = FALSE)
   # Restore factor levels
   datawizard::data_restoretype(trends, insight::get_data(model, verbose = FALSE))
