5a74cef604213da7022f239ba00f33dd8172b058
Daniel <mail@danielluedecke.de>
2025-01-13 00:39:11 +0100

    fix

--- R/visualisation_recipe_internal.R
+++ R/visualisation_recipe_internal.R
@@ -2,7 +2,7 @@
 
 
 #' @keywords internal
-.find_aes <- function(x, join_dots = TRUE) {
+.find_aes <- function(x) {
   data <- as.data.frame(x)
   att <- attributes(x)
   aes <- list(
@@ -65,11 +65,6 @@
     aes$grid <- stats::as.formula(paste(by[3], "~", paste(utils::tail(by, -3), collapse = "*")))
   }
 
-  # if range it preserved, no joined dots
-  if (isTRUE(attributes(x)$preserve_range) && is.numeric(data[[by[1]]])) {
-    join_dots <- FALSE
-  }
-
   # CI
   aes <- .find_aes_ci(aes, data)
 
@@ -103,12 +98,11 @@
                                   grid = NULL,
                                   join_dots = TRUE,
                                   ...) {
-  aes <- .find_aes(x, join_dots)
+  aes <- .find_aes(x)
   data <- aes$data
   aes <- aes$aes
   layers <- list()
   l <- 1
-  join_dots <- aes$join_dots
 
   # check whether point-geoms should be connected by lines
   do_not_join <- "grouplevel"
