c783f96cccb4c9cb2ee3e31c94f6b5bf3e4c94fa
Daniel <mail@danielluedecke.de>
2025-01-12 22:03:20 +0100

    add tests for print

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,6 +1,7 @@
 skip_on_os(c("mac", "linux"))
 skip_if_not_installed("emmeans")
 skip_if_not_installed("marginaleffects")
+skip_if_not_installed("ggeffects")
 
 test_that("estimate_slopes - print summary", {
   skip_if_not_installed("MASS")
@@ -18,6 +19,7 @@ test_that("estimate_slopes - print summary", {
   expect_snapshot(summary(deriv2), variant = "windows")
 })
 
+
 test_that("estimate_slopes - print regular", {
   data(iris)
   model <- lm(Sepal.Length ~ Petal.Length * Species, data = iris)
@@ -25,4 +27,29 @@ test_that("estimate_slopes - print regular", {
   expect_snapshot(print(slopes), variant = "windows")
   slopes2 <- estimate_slopes(model, trend = "Petal.Length", backend = "marginaleffects")
   expect_snapshot(print(slopes2), variant = "windows")
+})
+
+
+test_that("estimate_means - print multiple by's", {
+  data(efc, package = "ggeffects")
+
+  # make categorical
+  efc <- datawizard::to_factor(efc, c("c161sex", "c172code", "e16sex"))
+  fit <- lm(neg_c_7 ~ c12hour * barthtot * c161sex * c172code, data = efc)
+  out <- estimate_means(
+    fit,
+    c("c12hour", "c172code", "c161sex"),
+    length = 4,
+    backend = "marginaleffects"
+  )
+  expect_snapshot(print(out, table_width = Inf), variant = "windows")
+
+  fit <- lm(neg_c_7 ~ c12hour * barthtot * c161sex * c172code * e16sex, data = efc)
+  out <- estimate_means(
+    fit,
+    c("c12hour", "barthtot = [sd]", "c161sex", "c172code", "e16sex"),
+    backend = "marginaleffects",
+    length = 3
+  )
+  expect_snapshot(print(out, table_width = Inf), variant = "windows")
 })
