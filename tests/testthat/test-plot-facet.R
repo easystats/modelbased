d4468cc68f13405b3eae9cdd21c26856f480506f
Daniel <mail@danielluedecke.de>
2025-01-12 21:54:54 +0100

    add tests

---
+++ tests/testthat/test-plot-facet.R
@@ -0,0 +1,87 @@
+skip_on_os(c("mac", "solaris", "linux"))
+skip_if_not_installed("ggplot2")
+skip_if_not_installed("see")
+skip_if_not_installed("vdiffr")
+skip_if_not_installed("emmeans")
+skip_if_not_installed("marginaleffects")
+skip_if_not_installed("ggeffects")
+skip_on_cran()
+
+test_that("plots emmeans", {
+  data(efc, package = "ggeffects")
+
+  # make categorical
+  efc <- datawizard::to_factor(efc, c("c161sex", "c172code", "e16sex"))
+  fit <- lm(neg_c_7 ~ c12hour * barthtot * c161sex * c172code, data = efc)
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-1",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-2",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex", "barthtot"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-3",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot", "c161sex"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-4",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "barthtot", "c161sex", "c172code"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-5",
+    plot(pr, show_data = FALSE)
+  )
+
+  fit <- lm(neg_c_7 ~ c12hour * barthtot * c161sex * c172code * e16sex, data = efc)
+  set.seed(123)
+  pr <- estimate_means(fit, c("c12hour", "barthtot", "c161sex", "c172code", "e16sex"))
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-6",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c161sex", "c172code", "e16sex", "barthtot"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-7",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex", "barthtot"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-8",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot", "c161sex"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-9",
+    plot(pr, show_data = FALSE)
+  )
+
+  pr <- estimate_means(fit, c("c12hour", "e16sex", "c161sex", "c172code"))
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-10",
+    plot(pr, show_data = FALSE)
+  )
+})
