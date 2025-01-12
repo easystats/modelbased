e4f74999057709dcda1cb7461fbc59a55ff13357
Daniel <mail@danielluedecke.de>
2025-01-12 13:29:13 +0100

    fix

--- R/get_marginalcontrasts.R
+++ R/get_marginalcontrasts.R
@@ -79,7 +79,6 @@ get_marginalcontrasts <- function(model,
   attr(out, "p_adjust") <- p_adjust
   attr(out, "at") <- my_args$by
   attr(out, "by") <- my_args$by
-  attr(out, "focal_terms") <- at_specs$varname
   class(out) <- unique(c("marginaleffects_contrasts", class(out)))
   out
 }
