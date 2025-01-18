f5e1ee18c49f7f3f88de4a4ad9ac545ee6b77d43
Daniel <mail@danielluedecke.de>
2025-01-18 22:42:16 +0100

    add and fix tests

--- tests/testthat/test-mgcv.R
+++ tests/testthat/test-mgcv.R
@@ -1,64 +1,58 @@
-test_that("estimate_means - mgcv gam", {
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
-  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
+skip_if_not_installed("emmeans")
+skip_if_not_installed("marginaleffects")
+skip_if_not_installed("mgcv")
+skip_if_not_installed("gamm4")
 
-  estim <- suppressMessages(estimate_means(model))
+
+test_that("estimate_means - mgcv gam", {
+  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
+  estim <- suppressMessages(estimate_means(model, backend = "emmeans"))
   expect_identical(dim(estim), c(3L, 5L))
+  estim2 <- suppressMessages(estimate_means(model, backend = "marginaleffects"))
+  expect_identical(dim(estim2), c(3L, 7L))
+  expect_named(estim2, c("Species", "Mean", "SE", "CI_low", "CI_high", "t", "df"))
+  expect_equal(estim$Mean, estim2$Mean, tolerance = 1e-4)
 })
+
 
 test_that("estimate_contrasts - mgcv gam", {
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
   model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
-
-  estim <- suppressMessages(estimate_contrasts(model))
+  estim <- suppressMessages(estimate_contrasts(model, backend = "emmeans"))
   expect_identical(dim(estim), c(3L, 9L))
+  estim2 <- suppressMessages(estimate_contrasts(model, backend = "marginaleffects"))
+  expect_identical(dim(estim2), c(3L, 8L))
+  expect_named(estim2, c("Species", "Difference", "SE", "CI_low", "CI_high", "t", "df"))
+  expect_equal(estim$Difference, estim2$Difference, tolerance = 1e-4)
 })
 
-test_that("estimate_expectation - mgcv gam", {
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
-  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
 
+test_that("estimate_expectation - mgcv gam", {
+  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
   estim <- suppressMessages(estimate_expectation(model))
   expect_identical(dim(estim), c(150L, 7L))
 })
 
-test_that("estimate_link - mgcv gam", {
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
-  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
 
+test_that("estimate_link - mgcv gam", {
+  model <- mgcv::gam(Sepal.Length ~ Species + s(Sepal.Width, by = Species), data = iris)
   estim <- suppressMessages(estimate_link(model, preserve_range = FALSE))
   expect_equal(dim(estim), c(30, 6))
 })
+
 
 # model <- mgcv::gam(Sepal.Length ~ Petal.Length + s(Sepal.Width) + s(Species, bs = "fs"), data = iris)
 # estim <- estimate_link(model)
 
 test_that("estimate_expectation - mgcv gamm", {
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
   model <- mgcv::gamm(Sepal.Length ~ Petal.Length + s(Sepal.Width), random = list(Species = ~1), data = iris)
-
   estim <- suppressMessages(estimate_expectation(model))
   expect_equal(dim(estim), c(150, 8))
 })
 
+
 test_that("estimate_link - mgcv gamm", {
   skip_on_os("mac")
-  skip_if_not_installed("mgcv")
-  skip_if_not_installed("gamm4")
-  skip_if_not_installed("emmeans")
-
   model <- mgcv::gamm(Sepal.Length ~ Petal.Length + s(Sepal.Width), random = list(Species = ~1), data = iris)
-
   estim <- estimate_link(model, length = 4, verbose = FALSE)
   expect_identical(dim(estim), as.integer(c(16, 6)))
 })
