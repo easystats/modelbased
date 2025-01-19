77bcb003c1745e59ab7af67db3059be88e1ff1e3
Daniel <mail@danielluedecke.de>
2025-01-19 20:38:46 +0100

    skip test for old marginaleffects

--- tests/testthat/test-estimate_contrasts.R
+++ tests/testthat/test-estimate_contrasts.R
@@ -280,7 +280,7 @@ test_that("estimate_contrasts - dfs", {
 
 
 test_that("estimate_contrasts - marginaleffects", {
-  ## TODO: skip for marginaleffects 0.24.0?
+  skip_if_not_installed("marginaleffects", minimum_version = "0.24.1")
   skip_if_not_installed("Formula")
   skip_if_not_installed("ggeffects")
   data(coffee_data, package = "ggeffects")
