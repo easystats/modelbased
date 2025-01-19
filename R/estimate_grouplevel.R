36f85e929c7b62df1cbb7400e02e47115f597a62
Daniel <mail@danielluedecke.de>
2025-01-19 17:50:48 +0100

    Remove the internal utility `.to_numeric()`? (#358)
    Fixes #206

--- R/estimate_grouplevel.R
+++ R/estimate_grouplevel.R
@@ -80,7 +80,7 @@ estimate_grouplevel <- function(model, type = "random", ...) {
   random <- random[c("Group", "Level", names(random)[!names(random) %in% c("Group", "Level")])]
 
   # Sort
-  random <- random[order(random$Group, .to_numeric(random$Level), random$Parameter), ]
+  random <- random[order(random$Group, datawizard::coerce_to_numeric(random$Level), random$Parameter), ]
 
   # Clean
   row.names(random) <- NULL
