93a89c05b9c7ce3cc7e4d4f7e5334977d44f879d
Daniel <mail@danielluedecke.de>
2025-01-19 14:05:08 +0100

    fix footer

--- R/print_html.R
+++ R/print_html.R
@@ -8,6 +8,13 @@ print_html.estimate_contrasts <- function(x, full_labels = TRUE, ...) {
   # set alignment, left-align first and non-numerics
   align <- .align_columns(x, formatted_table)
 
+  # update footer
+  table_footer <- attributes(formatted_table)$table_footer
+  if (!is.null(table_footer)) {
+    table_footer <- insight::compact_character(strsplit(table_footer, "\\n")[[1]])
+    attr(formatted_table, "table_footer") <- paste(table_footer, collapse = "; ")
+  }
+
   insight::export_table(
     formatted_table,
     format = "html",
