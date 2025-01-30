8c9db0cda455c9b4439f841e9bccdf11f0ec6464
Daniel <mail@danielluedecke.de>
2025-01-30 15:07:51 +0100

    fix test

--- tests/testthat/test-estimate_contrasts_methods.R
+++ tests/testthat/test-estimate_contrasts_methods.R
@@ -7,24 +7,25 @@ test_that("estimate_contrasts - Random Effects Levels, pairwise", {
   # sample data set
   data(efc, package = "modelbased")
 
-  efc <- efc |>
-    # numeric to factors, set labels as levels
-    datawizard::to_factor(select = c("c161sex", "c172code", "c175empl")) |>
-    # recode age into three groups
-    datawizard::recode_values(
-      select = "c160age",
-      recode = list(`1` = "min:40", `2` = 41:64, `3` = "65:max")
-    ) |>
-    # rename variables
-    datawizard::data_rename(
-      select = c("c161sex", "c160age", "quol_5", "c175empl"),
-      replacement = c("gender", "age", "qol", "employed")
-    ) |>
-    # age into factor, set levels, and change labels for education
-    datawizard::data_modify(age = factor(age, labels = c("-40", "41-64", "65+")))
+  # numeric to factors, set labels as levels
+  d <- datawizard::to_factor(efc, select = c("c161sex", "c172code", "c175empl"))
+  # recode age into three groups
+  d <- datawizard::recode_values(
+    d,
+    select = "c160age",
+    recode = list(`1` = "min:40", `2` = 41:64, `3` = "65:max")
+  )
+  # rename variables
+  d <- datawizard::data_rename(
+    d,
+    select = c("c161sex", "c160age", "quol_5", "c175empl"),
+    replacement = c("gender", "age", "qol", "employed")
+  )
+  # age into factor, set levels, and change labels for education
+  d <- datawizard::data_modify(d age = factor(age, labels = c("-40", "41-64", "65+")))
 
   # Quality of Life score ranges from 0 to 25
-  m_null <- glmmTMB::glmmTMB(qol ~ 1 + (1 | gender:employed:age), data = efc)
+  m_null <- glmmTMB::glmmTMB(qol ~ 1 + (1 | gender:employed:age), data = d)
   estim <- estimate_relation(m_null, by = c("gender", "employed", "age"))
 
   # test errors
@@ -42,23 +43,25 @@ test_that("estimate_contrasts - Random Effects Levels, interaction", {
   # sample data set
   data(efc, package = "modelbased")
 
-  efc <- efc |>
-    # numeric to factors, set labels as levels
-    datawizard::to_factor(select = c("c161sex", "c172code", "c175empl")) |>
-    # recode age into three groups
-    datawizard::recode_values(
-      select = "c160age",
-      recode = list(`1` = "min:40", `2` = 41:64, `3` = "65:max")
-    ) |>
-    # rename variables
-    datawizard::data_rename(
-      select = c("c161sex", "c160age", "quol_5", "c175empl"),
-      replacement = c("gender", "age", "qol", "employed")
-    ) |>
-    # age into factor, set levels, and change labels for education
-    datawizard::data_modify(age = factor(age, labels = c("-40", "41-64", "65+")))
+  # numeric to factors, set labels as levels
+  d <- datawizard::to_factor(efc, select = c("c161sex", "c172code", "c175empl"))
+  # recode age into three groups
+  d <- datawizard::recode_values(
+    d,
+    select = "c160age",
+    recode = list(`1` = "min:40", `2` = 41:64, `3` = "65:max")
+  )
+  # rename variables
+  d <- datawizard::data_rename(
+    d,
+    select = c("c161sex", "c160age", "quol_5", "c175empl"),
+    replacement = c("gender", "age", "qol", "employed")
+  )
+  # age into factor, set levels, and change labels for education
+  d <- datawizard::data_modify(d age = factor(age, labels = c("-40", "41-64", "65+")))
 
-  m_null <- glmmTMB::glmmTMB(qol ~ 1 + (1 | employed:age), data = efc)
+  # Quality of Life score ranges from 0 to 25
+  m_null <- glmmTMB::glmmTMB(qol ~ 1 + (1 | gender:employed:age), data = d)
   estim <- estimate_relation(m_null, by = c("age", "employed"))
   expect_snapshot(print(estimate_contrasts(estim, contrast = c("age", "employed"), comparison = "interaction"), zap_small = TRUE, table_width = Inf))
 })
