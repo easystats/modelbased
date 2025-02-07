03aeb4ddfd45ebc56ad7f0a0dbf6fc75c7890b0d
Daniel <mail@danielluedecke.de>
2025-02-07 17:58:07 +0100

    fix

--- tests/testthat/test-table_footer.R
+++ tests/testthat/test-table_footer.R
@@ -5,7 +5,6 @@ test_that("table_footer", {
   data(efc, package = "modelbased")
   efc <- datawizard::to_factor(efc, c("c161sex", "c172code", "e16sex", "e42dep"))
   m <- lm(neg_c_7 ~ c12hour + barthtot + c161sex + e42dep + c172code, data = efc)
-  ## FIXME: need to add "adjusted_for" attribute to get_marginalmeans for estimate = "specific"
   out <- utils::capture.output(estimate_means(m, "c172code", estimate = "specific", verbose = FALSE))
   expect_identical(
     out[11],
