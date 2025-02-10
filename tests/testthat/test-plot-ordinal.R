50eed6dbd7aee829211e3a9841930445de734411
Daniel <mail@danielluedecke.de>
2025-02-10 21:57:36 +0100

    add tests

--- tests/testthat/test-plot-ordinal.R
+++ tests/testthat/test-plot-ordinal.R
@@ -4,6 +4,7 @@ skip_if_not_installed("see")
 skip_if_not_installed("vdiffr")
 skip_if_not_installed("marginaleffects")
 skip_if_not_installed("MASS")
+skip_if_not_installed("ordinal")
 skip_on_cran()
 
 test_that("plots ordinal", {
@@ -19,6 +20,26 @@ test_that("plots ordinal", {
   set.seed(123)
   vdiffr::expect_doppelganger(
     "plot-ordinal-2",
+    plot(out, show_data = FALSE)
+  )
+})
+
+
+test_that("plots package ordinal", {
+  data(wine, package = "ordinal")
+  m1 <- ordinal::clm(rating ~ temp * contact, data = wine)
+
+  out <- estimate_means(m1, by = "temp")
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-ordinal-3",
+    plot(out, show_data = FALSE)
+  )
+
+  out <- estimate_relation(m1, by = "temp")
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-ordinal-4",
     plot(out, show_data = FALSE)
   )
 })
