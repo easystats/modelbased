3255a3378b07ddda0947d5768c3535645f9e86e0
Daniel <mail@danielluedecke.de>
2025-02-10 21:51:13 +0100

    add test

--- tests/testthat/test-plot-ordinal.R
+++ tests/testthat/test-plot-ordinal.R
@@ -9,10 +9,16 @@ skip_on_cran()
 test_that("plots ordinal", {
   m <- MASS::polr(Species ~ Sepal.Width, data = iris)
   out <- estimate_means(m, by = "Sepal.Width")
-
   set.seed(123)
   vdiffr::expect_doppelganger(
     "plot-ordinal-1",
+    plot(out, show_data = FALSE)
+  )
+
+  out <- estimate_relation(m, by = "Sepal.Width")
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-ordinal-2",
     plot(out, show_data = FALSE)
   )
 })
