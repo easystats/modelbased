4fecec57c291c66100e3f1748e47157a77dd24d3
Daniel <mail@danielluedecke.de>
2025-02-10 12:29:55 +0100

    Update visualisation_recipe_internal.R

--- R/visualisation_recipe_internal.R
+++ R/visualisation_recipe_internal.R
@@ -25,7 +25,7 @@
   if (!isFALSE(numeric_as_discrete) && is.numeric(numeric_as_discrete)) {
     data[by] <- lapply(data[by], function(v) {
       if (is.numeric(v) && insight::n_unique(v) < numeric_as_discrete) {
-        v <- as.factor(insight::format_value(v, protect_integers = TRUE))
+        v <- factor(insight::format_value(v, protect_integers = TRUE), levels = unique(v))
       }
       v
     })
