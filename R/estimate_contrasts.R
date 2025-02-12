b987f108da5f0d3fee9785161ea633358f827cb1
Daniel <mail@danielluedecke.de>
2025-02-13 00:41:45 +0100

    Make new `estimate` option visible? (#392)
    * Make new `estimate` option visible?
    Fixes #391
    
    * desc
    
    * docs
    
    * minor
    
    * news
    
    * wording docs
    
    * fix
    
    * docs
    
    * docs
    
    * docs
    
    * typo
    
    * docs
    
    * styler
    
    * table titles
    
    * Update estimate_means_counterfactuals.md
    
    * fix
    
    * comment
    
    * filter
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update format.R
    
    * we need filter only for average
    
    * Update format.R
    
    * Update format.R
    
    * fix
    
    * Update get_marginalcontrasts.R
    
    * fix
    
    * by_token more flexible
    
    * add comment
    
    * comments
    
    * Update estimate_contrasts.md
    
    * fix
    
    * docs
    
    * rename
    
    * docs

--- R/estimate_contrasts.R
+++ R/estimate_contrasts.R
@@ -108,7 +108,7 @@ estimate_contrasts.default <- function(model,
                                        predict = NULL,
                                        ci = 0.95,
                                        comparison = "pairwise",
-                                       estimate = "average",
+                                       estimate = getOption("modelbased_estimate", "typical"),
                                        p_adjust = "none",
                                        transform = NULL,
                                        backend = getOption("modelbased_backend", "marginaleffects"),
@@ -147,10 +147,12 @@ estimate_contrasts.default <- function(model,
   info <- attributes(estimated)
 
   # Table formatting
-  attr(out, "table_title") <- c(ifelse(
-    estimate == "specific",
-    "Model-based Contrasts Analysis",
-    "Marginal Contrasts Analysis"
+  attr(out, "table_title") <- c(switch(
+    estimate,
+    specific = "Model-based Contrasts Analysis",
+    typical = "Marginal Contrasts Analysis",
+    average = "Averaged Contrasts Analysis",
+    population = "Counterfactual Contrasts Analysis (G-computation)"
   ), "blue")
   attr(out, "table_footer") <- .table_footer(
     out,
