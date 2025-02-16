3df0f2ba8854e5c9dd25207ee1b1c3968f6437cd
Daniel <mail@danielluedecke.de>
2025-02-16 12:11:24 +0100

    add test

--- tests/testthat/test-backtransform_invlink.R
+++ tests/testthat/test-backtransform_invlink.R
@@ -17,3 +17,30 @@ test_that("estimate_means correct inverse link for glmmTMB", {
   expect_equal(out1$CI_low, out2$CI_low, tolerance = 1e-4)
   expect_equal(out1$CI_high, out2$CI_high, tolerance = 1e-4)
 })
+
+
+test_that("estimate_means correct inverse link for glm", {
+  set.seed(5)
+  data <- data.frame(
+    outcome = rbinom(100, 1, 0.5),
+    var1 = rbinom(100, 1, 0.1),
+    var2 = rnorm(100, 10, 7)
+  )
+  m <- glm(
+    outcome ~ var1 * var2,
+    data = data,
+    family = binomial(link = "logit")
+  )
+
+  out <- estimate_relation(m, by = c("var1", "var2"))
+  expect_true(all(out$CI_low >= 0 & out$CI_low <= 1))
+  expect_true(all(out$CI_high >= 0 & out$CI_high <= 1))
+
+  out <- estimate_means(m, by = c("var1", "var2"))
+  expect_true(all(out$CI_low >= 0 & out$CI_low <= 1))
+  expect_true(all(out$CI_high >= 0 & out$CI_high <= 1))
+
+  out <- estimate_means(m, by = c("var1", "var2"), estimate = "population")
+  expect_true(all(out$CI_low >= 0 & out$CI_low <= 1))
+  expect_true(all(out$CI_high >= 0 & out$CI_high <= 1))
+})
