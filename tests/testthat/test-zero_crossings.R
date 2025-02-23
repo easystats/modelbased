d91e31990d874ea0299b419381c61bcc466202eb
Daniel <mail@danielluedecke.de>
2025-02-23 17:18:49 +0100

    typo

--- tests/testthat/test-zero_crossings.R
+++ tests/testthat/test-zero_crossings.R
@@ -5,5 +5,5 @@ test_that("zero_crossings", {
   out <- find_inversions(x)
   expect_equal(out, c(12.87478, 37.62484, 62.37516, 87.12522), tolerance = 1e-4)
   expect_true(is.na(zero_crossings(c(1, 2))))
-  excpect_error(zero_crossings(1), regex = "is not smaller")
+  expect_error(zero_crossings(1), regex = "is not smaller")
 })
