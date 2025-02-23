56b26ba1cfde00d2457115562d81619c0f873c53
Daniel <mail@danielluedecke.de>
2025-02-23 15:15:25 +0100

    No link-inv in pool_predictions, add tests (#418)
    * No link-inv in pool_predictions, add tests
    
    * add com,ment
    
    * remove duplicated test
    
    * add test

--- tests/testthat/test-standardize.R
+++ tests/testthat/test-standardize.R
@@ -34,3 +34,20 @@ test_that("standardize() - estimate_predicted", {
   expect_equal(as.vector(out1$Predicted), c(1.0906, -0.0577, -0.82805), tolerance = 1e-4)
   expect_equal(as.vector(out2$Predicted), estim$Predicted, tolerance = 1e-4)
 })
+
+
+test_that("standardize() - estimate_contrasts()", {
+  data(mtcars)
+
+  dat <- mtcars
+  dat$gear <- as.factor(dat$gear)
+  dat$cyl <- as.factor(dat$cyl)
+
+  # Simple
+  model <- lm(mpg ~ cyl, data = dat)
+  estim <- estimate_contrasts(model, "cyl", backend = "marginaleffects")
+  out1 <- standardize(estim)
+  out2 <- unstandardize(out1)
+  expect_equal(as.vector(out1$Difference), c(-1.14831, -1.91866, -0.77035), tolerance = 1e-4)
+  expect_equal(as.vector(out2$Difference), estim$Difference, tolerance = 1e-4)
+})
