0d01ca71cff84ca0beb3045635b6f7b71b993807
Daniel <mail@danielluedecke.de>
2025-03-09 19:29:16 +0100

    add test

---
+++ tests/testthat/test-estimate_means_bayesian.R
@@ -0,0 +1,30 @@
+skip_on_cran()
+skip_if_not_installed("marginaleffects")
+skip_if_not_installed("brms")
+skip_if_not_installed("curl")
+skip_if_offline()
+skip_if_not_installed("httr2")
+
+test_that("estimate_means() - posterior draws", {
+  m <- insight::download_model("brms_1")
+  skip_if(is.null(m))
+  out <- estimate_means(m, by = "wt", keep_iterations = 5)
+  expect_named(
+    attributes(out),
+    c(
+      "names", "class", "row.names", "at", "by", "focal_terms", "adjusted_for",
+      "predict", "estimate", "transform", "datagrid", "preserve_range",
+      "model_info", "posterior_draws", "keep_iterations", "table_title",
+      "table_footer", "model", "response", "ci", "backend", "coef_name"
+    )
+  )
+  expect_named(
+    out,
+    c(
+      "wt", "ROPE_CI", "Median", "CI_low", "CI_high", "pd", "ROPE_low",
+      "ROPE_high", "ROPE_Percentage", "iter_1", "iter_2", "iter_3",
+      "iter_4", "iter_5"
+    )
+  )
+  expect_identical(dim(out), c(10L, 14L))
+})
