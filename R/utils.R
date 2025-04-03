8eea7d44527c0c686cb461eaa9bce4d21671cf0e
Daniel <mail@danielluedecke.de>
2025-04-03 17:30:38 +0200

    minor

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
 .brms_aux_elements <- function() {
   c(
     "sigma", "mu", "nu", "shape", "beta", "phi", "hu", "ndt", "zoi", "coi",
-    "kappa", "bias", "bs", "zi", "alpha", "xi"
+    "kappa", "bias", "bs", "zi", "alpha", "xi", "delta", "k"
   )
 }
 
