baafde8db8a58b97b57a2f1c547375d76d48364c
Daniel <mail@danielluedecke.de>
2025-09-23 18:27:05 +0200

    air styler

--- tests/testthat/test-equivalence.R
+++ tests/testthat/test-equivalence.R
@@ -7,13 +7,17 @@ test_that("estimate_means() - equivalence test", {
   data(mtcars)
   mtcars$mpg <- datawizard::normalize(mtcars$mpg, include_bounds = FALSE)
 
-  mod <- glmmTMB::glmmTMB(mpg ~ as.factor(gear) + (1|cyl), data = mtcars, family = glmmTMB::beta_family())
+  mod <- glmmTMB::glmmTMB(
+    mpg ~ as.factor(gear) + (1 | cyl),
+    data = mtcars,
+    family = glmmTMB::beta_family()
+  )
   out <- estimate_means(mod, "gear", equivalence = c(0.3, 0.5))
   expect_named(out, c("gear", "Proportion", "SE", "CI_low", "CI_high", "z", "p_Equivalence"))
   expect_equal(out$p_Equivalence, c(0.26701, 0.50868, 0.39303), tolerance = 1e-4)
   expect_identical(attributes(out)$equivalence, c(0.3, 0.5))
 
-  mod <- glmmTMB::glmmTMB(mpg ~ wt + (1|cyl), data = mtcars, family = glmmTMB::beta_family())
+  mod <- glmmTMB::glmmTMB(mpg ~ wt + (1 | cyl), data = mtcars, family = glmmTMB::beta_family())
   out <- estimate_slopes(mod, "wt", equivalence = c(-0.2, 0.2))
   expect_named(out, c("gear", "Proportion", "SE", "CI_low", "CI_high", "z", "p_Equivalence"))
   expect_equal(out$p_Equivalence, 0.9872101, tolerance = 1e-4)
