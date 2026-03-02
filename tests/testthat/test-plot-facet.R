02803daae58936b7079665db1c3e4921f7433cd9
Daniel <mail@danielluedecke.de>
2026-03-02 16:08:48 +0100

    Plot estimated means -  why connecting lines between groups? (#607)
    * Plot estimated means -  why connecting lines between groups?
    Fixes #603
    
    * update snaps
    
    * add snaps
    
    * fix test
    
    * update snaps/tests

--- tests/testthat/test-plot-facet.R
+++ tests/testthat/test-plot-facet.R
@@ -15,74 +15,44 @@ test_that("plots facets", {
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-1",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-1", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-2",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-2", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex", "barthtot"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-3",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-3", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot", "c161sex"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-4",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-4", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "barthtot", "c161sex", "c172code"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-5",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-5", plot(pr, show_data = FALSE))
 
   fit <- lm(neg_c_7 ~ c12hour * barthtot * c161sex * c172code * e16sex, data = efc)
   set.seed(123)
   pr <- estimate_means(fit, c("c12hour", "barthtot", "c161sex", "c172code", "e16sex"))
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-6",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-6", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c161sex", "c172code", "e16sex", "barthtot"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-7",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-7", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "c161sex", "barthtot"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-8",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-8", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "c172code", "barthtot", "c161sex"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-9",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-9", plot(pr, show_data = FALSE))
 
   pr <- estimate_means(fit, c("c12hour", "e16sex", "c161sex", "c172code"))
   set.seed(123)
-  vdiffr::expect_doppelganger(
-    "plot-interaction-facets-10",
-    plot(pr, show_data = FALSE)
-  )
+  vdiffr::expect_doppelganger("plot-interaction-facets-10", plot(pr, show_data = FALSE))
 })
 
 
@@ -98,6 +68,11 @@ test_that("plots facets", {
     "plot-interaction-facets-cat-1",
     plot(pr, show_data = FALSE)
   )
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-cat-1-lines",
+    plot(pr, show_data = FALSE, join_dots = TRUE)
+  )
 
   fit <- lm(neg_c_7 ~ c161sex * c172code * e16sex * nur_pst * negc7d, data = efc)
   pr <- estimate_means(fit, c("c161sex", "c172code", "e16sex", "nur_pst", "negc7d"))
@@ -105,6 +80,12 @@ test_that("plots facets", {
   vdiffr::expect_doppelganger(
     "plot-interaction-facets-cat-2",
     plot(pr, show_data = FALSE)
+  )
+  # connecting lines
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    "plot-interaction-facets-cat-3-lines",
+    plot(pr, show_data = FALSE, join_dots = TRUE)
   )
   # no connecting lines
   set.seed(123)
@@ -121,7 +102,11 @@ test_that("plots facets, emmeans", {
   efc <- datawizard::to_factor(efc, c("c161sex", "c172code", "e16sex", "nur_pst"))
 
   fit <- lm(neg_c_7 ~ c161sex * c172code * e16sex * nur_pst, data = efc)
-  pr <- estimate_means(fit, c("c161sex", "c172code", "e16sex", "nur_pst"), backend = "emmeans")
+  pr <- estimate_means(
+    fit,
+    c("c161sex", "c172code", "e16sex", "nur_pst"),
+    backend = "emmeans"
+  )
   set.seed(123)
   vdiffr::expect_doppelganger(
     "plot-interaction-facets-cat-4emmeans",
@@ -129,11 +114,16 @@ test_that("plots facets, emmeans", {
   )
 
   fit <- lm(neg_c_7 ~ c161sex * c172code * e16sex * nur_pst * negc7d, data = efc)
-  pr <- estimate_means(fit, c("c161sex", "c172code", "e16sex", "nur_pst", "negc7d"), backend = "emmeans")
+  pr <- estimate_means(
+    fit,
+    c("c161sex", "c172code", "e16sex", "nur_pst", "negc7d"),
+    backend = "emmeans"
+  )
+  # connecting lines
   set.seed(123)
   vdiffr::expect_doppelganger(
     "plot-interaction-facets-cat-5emmeans",
-    plot(pr, show_data = FALSE)
+    plot(pr, show_data = FALSE, join_dots = TRUE)
   )
   # no connecting lines
   set.seed(123)
