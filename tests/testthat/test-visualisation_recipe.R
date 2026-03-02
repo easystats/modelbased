de2300f9b3c8e82957b57262ae5ac83896e30479
Daniel <mail@danielluedecke.de>
2026-03-02 14:59:23 +0100

    fix test

--- tests/testthat/test-visualisation_recipe.R
+++ tests/testthat/test-visualisation_recipe.R
@@ -39,7 +39,6 @@ test_that("visualization_recipe", {
   expect_identical(aes$x, "Species")
   expect_identical(aes$color, "fac")
 
-
   x <- estimate_means(model, by = c("Species", "Sepal.Width", "fac"))
   # plot(modelbased:::.visualization_recipe(x))
   aes <- modelbased:::.find_aes(x)$aes
@@ -52,37 +51,75 @@ test_that("visualization_recipe", {
   dat <- data.frame(y = rpois(100, 3), fa = gl(4, 20, 100))
   dat_glm <- glm(y ~ fa, data = dat, family = poisson(link = "log"))
   x <- estimate_means(dat_glm, "fa", backend = "emmeans")
-  vr <- visualisation_recipe(x, show_data = TRUE)
+  vr <- visualisation_recipe(x, show_data = TRUE, join_dots = TRUE)
   expect_identical(vr$l1$aes, list(y = "y", x = "fa", color = NULL, alpha = NULL))
-  expect_identical(vr$l2$aes, list(y = "Rate", x = "fa", color = NULL, alpha = NULL, group = ".group"))
+  expect_identical(
+    vr$l2$aes,
+    list(y = "Rate", x = "fa", color = NULL, alpha = NULL, group = ".group")
+  )
   expect_identical(
     vr$l3$aes,
     list(
-      y = "Rate", x = "fa", ymin = "CI_low", ymax = "CI_high",
-      color = NULL, group = ".group", alpha = NULL
+      y = "Rate",
+      x = "fa",
+      ymin = "CI_low",
+      ymax = "CI_high",
+      color = NULL,
+      group = ".group",
+      alpha = NULL
     )
   )
-  x <- estimate_means(dat_glm, "fa", backend = "marginaleffects")
-  vr <- visualisation_recipe(x, show_data = TRUE)
+  vr <- visualisation_recipe(x, show_data = TRUE, join_dots = FALSE)
   expect_identical(vr$l1$aes, list(y = "y", x = "fa", color = NULL, alpha = NULL))
-  expect_identical(vr$l2$aes, list(y = "Mean", x = "fa", color = NULL, alpha = NULL, group = ".group"))
-  expect_identical(
-    vr$l3$aes,
-    list(
-      y = "Mean", x = "fa", ymin = "CI_low", ymax = "CI_high",
-      color = NULL, group = ".group", alpha = NULL
-    )
-  )
-  vr <- visualisation_recipe(x, show_data = FALSE)
-  expect_identical(vr$l1$aes, list(y = "Mean", x = "fa", color = NULL, alpha = NULL, group = ".group"))
   expect_identical(
     vr$l2$aes,
     list(
-      y = "Mean", x = "fa", ymin = "CI_low", ymax = "CI_high",
-      color = NULL, group = ".group", alpha = NULL
+      y = "Rate",
+      x = "fa",
+      ymin = "CI_low",
+      ymax = "CI_high",
+      color = NULL,
+      group = ".group",
+      alpha = NULL
     )
   )
-
+  expect_null(vr$l3$aes)
+  x <- estimate_means(dat_glm, "fa", backend = "marginaleffects")
+  vr <- visualisation_recipe(x, show_data = TRUE, join_dots = TRUE)
+  expect_identical(vr$l1$aes, list(y = "y", x = "fa", color = NULL, alpha = NULL))
+  expect_identical(
+    vr$l2$aes,
+    list(y = "Mean", x = "fa", color = NULL, alpha = NULL, group = ".group")
+  )
+  expect_identical(
+    vr$l3$aes,
+    list(
+      y = "Mean",
+      x = "fa",
+      ymin = "CI_low",
+      ymax = "CI_high",
+      color = NULL,
+      group = ".group",
+      alpha = NULL
+    )
+  )
+  vr <- visualisation_recipe(x, show_data = FALSE, join_dots = TRUE)
+  expect_identical(
+    vr$l1$aes,
+    list(y = "Mean", x = "fa", color = NULL, alpha = NULL, group = ".group")
+  )
+  expect_identical(
+    vr$l2$aes,
+    list(
+      y = "Mean",
+      x = "fa",
+      ymin = "CI_low",
+      ymax = "CI_high",
+      color = NULL,
+      group = ".group",
+      alpha = NULL
+    )
+  )
 
   # Estimate predictions ---------------------------
   x <- estimate_relation(model, by = "Sepal.Width")
@@ -158,7 +195,12 @@ test_that("visualization_recipe", {
   expect_identical(aes$x, "Species")
   expect_null(aes$color)
 
-  x <- estimate_slopes(model, trend = "Species", by = "Sepal.Width", backend = "marginaleffects")
+  x <- estimate_slopes(
+    model,
+    trend = "Species",
+    by = "Sepal.Width",
+    backend = "marginaleffects"
+  )
   aes <- modelbased:::.find_aes(x)$aes
   expect_identical(aes$y, "Slope")
   expect_identical(aes$x, "Sepal.Width")
