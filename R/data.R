a95b51ae45b5433b25c5abe4861da9ac23e4dfe0
Daniel <mail@danielluedecke.de>
2026-09-14 12:11:46 +0200

    Add sample dataset

--- R/data.R
+++ R/data.R
@@ -65,3 +65,36 @@ NULL
 #'   ("`coffee"` or `"control"`).
 #' - `alertness`: The participant's alertness score.
 NULL
+
+
+#' @docType data
+#' @title Sample dataset from the SOMACROSS project
+#' @name stigma
+#' @keywords data
+#'
+#' @description A sample data set from the SOMACROSS project (Development and
+#' Persistence of Somatic Symptoms). This is a random, anonymized subset of the
+#' project data set.
+#'
+#' The data consists of nine variables from 1200 observations (400 patients at
+#' three time points):
+#'
+#' - `patid`: A unique identifier for each participant
+#' - `disease_group`: Classification of main disease from patient, e.g.
+#'   primary Sclerosing Cholangitis, chronic kidney disease (CKD), Ulcerative
+#'   colitis, Irritable Bowel Syndrome (IBS)	and others.
+#' - `phq15`: Total score of the PHQ-15 questionnaire (Patient Health
+#'   Questionnaire-15), measuring the somatic symptoms severity
+#' - `stigma_unreal`: Question regarding perceived stigma related to somatic
+#'   symptoms, asking "Most people believe that my symptoms are not a real
+#'   illness". Answer categories ranged from `"strongly disagree"`,
+#'   `"disagree"`, `"neutral"`, `"agree"`, and `"strongly agree"`.
+#'   `"I have no complaints"` was the residual category for patients without
+#'   specific somatic symptoms.
+#' - `time3`: Measurement point (baseline, and two followu-ups)
+#' - `education_casmin`: Educational level, measured according to the CASMIN
+#'   classification.
+#' - `sex`: The participant's sex
+#' - `migration_history`: History of migrations from patients
+#' - `age_z`: Age of patient, standardized
+NULL
