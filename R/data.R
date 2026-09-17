9c8a6620d855543f36e30da3459fefc58b7eef51
Daniel <mail@danielluedecke.de>
2026-09-17 12:35:47 +0200

    rename variables

--- R/data.R
+++ R/data.R
@@ -78,22 +78,21 @@ NULL
 #' The data consists of nine variables from 1140 observations (380 patients at
 #' three time points):
 #'
-#' - `patid`: A unique identifier for each participant
+#' - `ID`: A unique identifier for each participant
 #' - `disease_group`: Classification of main disease from patient, e.g. chronic
 #'   kidney disease (CKD), Ulcerative colitis, Irritable Bowel Syndrome (IBS)
 #'   and others.
 #' - `phq15`: Total score of the PHQ-15 questionnaire (Patient Health
 #'   Questionnaire-15), measuring the somatic symptoms severity
-#' - `stigma_unreal`: Question regarding perceived stigma related to somatic
-#'   symptoms, asking "Most people believe that my symptoms are not a real
+#' - `symptoms_unreal`: Question regarding perceived stigma related to somatic
+#'   symptoms, asking "People believe that my symptoms are not a real
 #'   illness". Answer categories ranged from `"strongly disagree"`,
 #'   `"disagree"`, `"neutral"`, `"agree"`, and `"strongly agree"`.
 #'   `"I have no complaints"` was the residual category for patients without
 #'   specific somatic symptoms.
 #' - `time3`: Measurement point (baseline, and two follow-ups)
-#' - `education_casmin`: Educational level, measured according to the CASMIN
+#' - `education`: Educational level, measured according to the CASMIN
 #'   classification.
 #' - `sex`: The participant's sex
-#' - `migration_history`: History of migration from patients
 #' - `age_z`: Age of patient, standardized
 NULL
